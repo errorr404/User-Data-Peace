c37b0735dfd946f1ade62e0cc1ca833a54c69b6d
codebrahma <dkbishwash@gmail.com>
2019-11-14 19:06:03 +0530

    feat: Added navbar
    Added navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { withRouter } from 'react-router-dom';
+import './navbar.scss';
 
 const Navbar = (props) => {
-  const { xyz } = props;
+  const { match } = props;
+  const showBackArrow = match.url.includes('user');
+
+  const handleIconClick = () => {
+    if (showBackArrow) props.history.push('/');
+  };
   return (
-    <div>
+    <div className="navbar">
       <div>
-        <FontAwesomeIcon icon="bars" />
+        <FontAwesomeIcon
+          icon={showBackArrow ? 'arrow-left' : 'bars'}
+          className="navbar__icon"
+          onClick={handleIconClick}
+        />
       </div>
-      <span>Data Peace</span>
+      <span className="navbar__text">Data Peace</span>
     </div>
   );
 };
 
-export default Navbar;
+export default withRouter(Navbar);
